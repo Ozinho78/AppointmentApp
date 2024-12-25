deea144c1608285366b20dd77bd7fa3226eb1b8d
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2024-12-25 20:19:24 +0100

    appointment added

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -8,10 +8,34 @@ import { Appointment } from '../models/appointment';
 })
 export class AppointmentListComponent {
 
-  appointment: Appointment = {
-    id: 1,
-    title: "Take a walk",
-    date: new Date('2023-07-30')
+  // appointment: Appointment = {
+  //   id: 1,
+  //   title: "Take a walk",
+  //   date: new Date('2023-07-30')
+  // }
+
+  newAppointmentTitle: string = "";
+  newAppointmentDate: Date = new Date();
+
+  appointments: Appointment[] = [];
+
+  addAppointment(){
+    // alert(this.newAppointmentTitle + " " + this.newAppointmentDate);
+    if(this.newAppointmentTitle.trim().length && this.newAppointmentDate){
+      let newAppointment: Appointment = {
+        id: Date.now(),        // milliseconds since 01.01.1970
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate
+      }
+    
+    this.appointments.push(newAppointment);
+
+    this.newAppointmentTitle = "";
+    this.newAppointmentDate = new Date();
+
+    alert(this.appointments.length);
+    }
   }
+
 
 }
